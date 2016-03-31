8af58babeb58e60b362777ef7a1294e6785fe29d
brew <76052198@qq.com>
2016-03-31 17:29:39 +0800

    change api to 2.1.2, bugs for protobuf bean download

--- oapi/src/main/java/onight/tfw/async/AsyncPBActor.java
+++ oapi/src/main/java/onight/tfw/async/AsyncPBActor.java
@@ -23,7 +23,7 @@ public abstract class AsyncPBActor<T extends Message> extends PBActor<T> {
 	@Override
 	public void doWeb(final HttpServletRequest req, final HttpServletResponse resp, final FramePacket pack) throws IOException {
 		resp.setCharacterEncoding("UTF-8");
-		resp.setHeader("Content-type", "application/json;charset=UTF-8");  
+		resp.setHeader("Content-type", "application/json;charset=UTF-8");
 
 		final AsyncContext asyncContext = req.startAsync();
 		asyncContext.start(new Runnable() {
@@ -40,26 +40,38 @@ public abstract class AsyncPBActor<T extends Message> extends PBActor<T> {
 								}
 								retpack.getExtHead().buildFor(resp);
 								if (retpack.getFbody() != null & retpack.getFbody() instanceof Message) {
-									Message msg = (Message) retpack.getFbody();
-									String str = new JsonPBFormat().printToString(msg);
-									retpack.getFixHead().genBytes();
-									String ret = "{\"fh\":\"" + (new String(retpack.getFixHead().genBytes())) + "\""//
-											+ ",\"eh\":" + new String(SerializerUtil.toBytes(jsons.serialize(retpack.getExtHead().getVkvs()))) + "" //
-											+ ",\"body\":" + str + "" + "}";
+									if (retpack.getFixHead().getEnctype() == 'P') {
+										byte[] bodyb = retpack.genBodyBytes();
+										byte[] extb = retpack.genExtBytes(); 
+										retpack.getFixHead().setExtsize(extb.length);
+										retpack.getFixHead().setBodysize(bodyb.length);
 
-									resp.getOutputStream().write(ret.getBytes("UTF-8"));
+										resp.getOutputStream().write(retpack.getFixHead().genBytes());
+										resp.getOutputStream().write(extb);
+										resp.getOutputStream().write(bodyb);
+									}else{
+										Message msg = (Message) retpack.getFbody();
+										String str = new JsonPBFormat().printToString(msg);
+										retpack.getFixHead().genBytes();
+										String ret = "{\"fh\":\"" + (new String(retpack.getFixHead().genBytes())) + "\""//
+												+ ",\"eh\":" + new String(SerializerUtil.toBytes(jsons.serialize(retpack.getExtHead().getVkvs()))) + "" //
+												+ ",\"body\":" + str + "" + "}";
 
-								} else if (retpack.getFbody()!=null&&retpack.getFixHead().getEnctype() == SerializerFactory.SERIALIZER_JSON ) {
+										resp.getOutputStream().write(ret.getBytes("UTF-8"));
+										
+									}
+
+								} else if (retpack.getFbody() != null && retpack.getFixHead().getEnctype() == SerializerFactory.SERIALIZER_JSON) {
 									String ret = "{\"fh\":\"" + (new String(retpack.getFixHead().genBytes())) + "\""//
-											+ ",\"eh\":" + new String((byte[])(jsons.serialize(retpack.getExtHead().getVkvs()))) + "" //
-											+ ",\"body\":" + new String((byte[])jsons.serialize(retpack.getFbody())) + "" + "}";
+											+ ",\"eh\":" + new String((byte[]) (jsons.serialize(retpack.getExtHead().getVkvs()))) + "" //
+											+ ",\"body\":" + new String((byte[]) jsons.serialize(retpack.getFbody())) + "" + "}";
 
 									resp.getOutputStream().write(ret.getBytes("UTF-8"));
 
 									//
 								} else if (retpack.getFixHead().getEnctype() == SerializerFactory.SERIALIZER_JSON && retpack.getBody() != null) {
 									String ret = "{\"fh\":\"" + (new String(retpack.getFixHead().genBytes())) + "\""//
-											+ ",\"eh\":" + new String((byte[])(jsons.serialize(retpack.getExtHead().getVkvs()))) + "" //
+											+ ",\"eh\":" + new String((byte[]) (jsons.serialize(retpack.getExtHead().getVkvs()))) + "" //
 											+ ",\"body\":" + new String(retpack.getBody()) + "" + "}";
 
 									resp.getOutputStream().write(ret.getBytes("UTF-8"));
@@ -91,9 +103,7 @@ public abstract class AsyncPBActor<T extends Message> extends PBActor<T> {
 					} catch (IOException e1) {
 						// e1.printStackTrace();
 						log.debug("error response:", e);
-					}
-					finally
-					{
+					} finally {
 						asyncContext.complete();
 					}
 				}
